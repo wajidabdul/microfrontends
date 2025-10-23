c918835362a654cc36aac9c1773245a7fd6bb5ce
Abdul Wajid <abdulwajid@Abduls-MacBook-Air.local>
2025-10-23 20:45:16 +0530

    config changes

--- packages/container/src/index.js
+++ packages/container/src/index.js
@@ -1,1 +1,16 @@
 import("./bootstrap");
+
+// Minor Changes in AWS CloudFront UI
+// In the upcoming lecture, we will be creating our CloudFront distribution. The UI for this service has changed slightly, so, here are some notes to try and avoid confusion:
+
+// 1. In the video there are two choices, Web and RTMP. Since the RTMP method has been removed, you no longer have to explicitly select Web as it is the default and only delivery method.
+
+// 2. Distribution settings have been renamed to Settings.
+
+// 3. The SSL certificate fields look different and no longer show that the default CloudFront certificate is selected. This is ok, the default certificate is still being used and nothing needs to be done or changed.
+
+// All other AWS settings and configurations should track what is shown in the videos through sections 7 and 8.
+
+// There is an AWS cheatsheet with all steps to create an S3 bucket, CloudFront distribution, and IAM user that will be regularly updated here:
+
+// https://www.udemy.com/course/microfrontend-course/learn/lecture/33274448#questions
